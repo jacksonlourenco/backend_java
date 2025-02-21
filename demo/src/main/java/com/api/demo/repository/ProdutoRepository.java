fca9613d68080afa4a0099d155c75c42ae033065
Jackson Andre Lourenco <107936025+jacksonlourenco@users.noreply.github.com>
2025-02-21 11:27:07 -0300

    CRUD Completo
    Testado todas as rotas locais. OK

---
+++ demo/src/main/java/com/api/demo/repository/ProdutoRepository.java
@@ -0,0 +1,56 @@
+package com.api.demo.repository;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.api.demo.model.Produto;
+
+@Repository
+public class ProdutoRepository {
+    
+    private List<Produto> produtos = new ArrayList<>();
+    private Integer ultimoId = 0;
+
+    public List<Produto> obterTodos(){
+        return produtos;
+    }
+
+    public Optional<Produto> obterPorId(Integer id){
+        return produtos
+            .stream()
+            .filter(produto -> produto.getId() == id)
+            .findFirst();
+    }
+
+    public Produto adicionar(Produto produto){
+        ultimoId++;
+
+        produto.setId(ultimoId);
+        produtos.add(produto);
+
+        return produto;
+    }
+
+    public void deletar(Integer id){
+
+        produtos.removeIf(produto -> produto.getId() == id);
+    }
+
+    public Produto atualizar(Produto produto){
+        Optional<Produto> produtoEncontrado = obterPorId(produto.getId());
+
+        if(produtoEncontrado.isEmpty()){
+
+            throw new InputMismatchException("Produto não encontrado!");
+        }
+
+        deletar(produto.getId());
+        produtos.add(produto);
+
+        return produto;
+    }
+}
